2b27001475371f15150eb0b8f27b683f275deb96
Riaan van der Westhuizen <riaan@ordercloud.co.za>
2024-09-26 14:08:22 +0200

    SideMenu component

---
+++ src/Share/Share.UI/SideMenu/SideMenu.razor.cs
@@ -0,0 +1,6 @@
+﻿namespace Share;
+
+public partial class SideMenu
+{
+    [Parameter] public RenderFragment ChildContent { get; set; } = default!;
+}
---
+++ src/Share/Share.UI/SideMenu/SideMenuItem.razor.cs
@@ -0,0 +1,31 @@
+﻿namespace Share;
+
+public partial class SideMenuItem
+{
+    #region Members
+    private readonly NavigationManager _navigation;
+
+    [CascadingParameter(Name = "responsive")] private Responsive Responsive { get; set; } = default!;
+    [CascadingParameter(Name = "sideBar")] private SideMenu SideMenu { get; set; } = default!;
+    #endregion
+
+    #region Constructor
+    public SideMenuItem(NavigationManager navigation)
+    {
+        _navigation = navigation;
+    }
+    #endregion
+
+    #region Public
+    [Parameter] public RenderFragment ChildContent { get; set; } = default!;
+    [Parameter] public string Label { get; set; } = default!;
+    [Parameter] public string Uri { get; set; } = default!;
+    #endregion
+
+    #region Private
+    private void OnOpenSelect()
+    {
+        _navigation.NavigateTo(Uri);
+    }
+    #endregion
+}
