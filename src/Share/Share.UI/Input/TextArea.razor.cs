5044ffd8cc61c5b925f5efbeea08336eaf09af34
Riaan van der Westhuizen <riaan@ordercloud.co.za>
2024-09-27 16:46:49 +0200

    Text Area component

---
+++ src/Share/Share.UI/Input/TextArea.razor.cs
@@ -0,0 +1,35 @@
+﻿namespace Share;
+
+public partial class TextArea
+{
+    #region Members
+    [CascadingParameter(Name = "form")] private Form InputForm { get; set; } = default!;
+    #endregion
+
+    #region Public
+    [Parameter] public string Value { get; set; } = "";
+    [Parameter] public EventCallback<string> ValueChanged { get; set; }
+    #endregion
+
+    #region Private        
+    private void OnValueChange(ChangeEventArgs e)
+    {
+        Value = e.Value?.ToString() ?? "";
+        _ = ValueChanged.InvokeAsync(Value);
+    }
+    private string LabelText()
+    {
+        //if (Optional) return $"{Label} - Optional";
+        //else return Label;
+
+        return Label;
+    }
+    #endregion
+
+    #region Protected
+    protected override void OnInitialized()
+    {
+        InputForm?.AddInput(this);
+    }
+    #endregion
+}
