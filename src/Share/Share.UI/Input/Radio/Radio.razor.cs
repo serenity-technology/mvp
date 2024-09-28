9d66e6642c8815698781c677f0e2ef17b1071c86
Riaan van der Westhuizen <riaan@ordercloud.co.za>
2024-09-28 12:18:47 +0200

    Radion component

---
+++ src/Share/Share.UI/Input/Radio/Radio.razor.cs
@@ -0,0 +1,52 @@
+﻿namespace Share;
+
+public partial class Radio<TValue> : Input
+{
+    #region Members
+    [CascadingParameter(Name = "form")] private Form InputForm { get; set; } = default!;
+    #endregion
+
+    #region Public
+    [Parameter] public TValue Value { get; set; } = default!;
+    [Parameter] public EventCallback<TValue> ValueChanged { get; set; }
+    [Parameter] public IEnumerable<IOption<TValue>> Options { get; set; } = default!;
+
+    public TValue CurrentValue
+    {
+        get => Value;
+        set
+        {
+            var hasChanged = !EqualityComparer<TValue>.Default.Equals(value, Value);
+            if (hasChanged)
+            {
+                Value = value;
+                _ = ValueChanged.InvokeAsync(value);
+            }
+        }
+    }
+    #endregion
+
+    #region Private
+    private string InternalLabelClass
+    {
+        get
+        {
+            var css = new ElementClass();
+
+            if (Disabled)
+                css.Add("text-sm font-medium text-gray-700");
+            else
+                css.Add("text-sm font-medium text-gray-400");
+
+            return css;
+        }
+    }
+    #endregion
+
+    #region Protected
+    protected override void OnInitialized()
+    {
+        InputForm?.AddInput(this);
+    }
+    #endregion
+}
---
+++ src/Share/Share.UI/Input/Radio/RadioOption.razor.cs
@@ -0,0 +1,49 @@
+﻿namespace Share;
+
+public partial class RadioOption<TValue> : Component
+{
+    #region Public
+    [Parameter] public IOption<TValue> Option { get; set; } = default!;
+    [Parameter] public bool Checked { get; set; } = false;
+    [Parameter] public bool Disabled { get; set; } = false;
+    #endregion
+
+    #region Private
+    [CascadingParameter(Name = "radioGroup")] private Radio<TValue> RadioGroup { get; set; } = default!;
+
+    private void OnItemSelect()
+    {
+        RadioGroup.CurrentValue = Option.Value;
+    }
+
+    private string InternalInputClass
+    {
+        get
+        {
+            var css = new ElementClass();
+
+            if (Disabled)
+                css.Add("h-4 w-4 text-gray-400 border-gray-300");
+            else
+                css.Add("focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300");
+
+            return css;
+        }
+    }
+
+    private string InternalLabelClass
+    {
+        get
+        {
+            var css = new ElementClass();
+
+            if (Disabled)
+                css.Add("ml-3 block text-sm font-medium text-gray-400");
+            else
+                css.Add("ml-3 block text-sm font-medium text-gray-500");
+
+            return css;
+        }
+    }
+    #endregion
+}
