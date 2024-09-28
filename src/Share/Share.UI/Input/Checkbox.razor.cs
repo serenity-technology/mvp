38a8939af75f6b73206cf62f978b3140bad82081
Riaan van der Westhuizen <riaan@ordercloud.co.za>
2024-09-28 11:25:32 +0200

    Checkbox component

---
+++ src/Share/Share.UI/Input/Checkbox.razor.cs
@@ -0,0 +1,44 @@
+﻿namespace Share;
+
+public partial class Checkbox
+{
+    #region Members
+    [CascadingParameter(Name = "form")] private Form InputForm { get; set; } = default!;
+    #endregion
+
+    #region Public
+    [Parameter] public bool Value { get; set; }
+    [Parameter] public EventCallback<bool> ValueChanged { get; set; }
+    #endregion
+
+    #region Private
+    private void OnValueChange(ChangeEventArgs e)
+    {
+        var value = e.Value?.ToString() ?? "";
+        if (bool.TryParse(value, out var checkedValue))
+        {
+            Value = checkedValue;
+        }
+        else
+            Value = false;
+
+        _ = ValueChanged.InvokeAsync(Value);
+    }
+
+    private void ToggleChecked()
+    {
+        if (!Disabled)
+        {
+            Value = !Value;
+            _ = ValueChanged.InvokeAsync(Value);
+        }
+    }
+    #endregion
+
+    #region Protected
+    protected override void OnInitialized()
+    {
+        InputForm?.AddInput(this);
+    }
+    #endregion
+}
